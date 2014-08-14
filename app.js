3ed6428f8430bff7dff2ea2aea73ad7a4e5f0f67
Phil Spitler (PRS) <prs@sproutkey.com>
2014-08-14 09:12:40 -0400

    cleaned up some unused routes

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
@@ -22,8 +20,6 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 app.use('/api', api);
 
 /// catch 404 and forward to error handler
