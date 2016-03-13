cce97df06a129dbae47c7cd806b73e10deb867bb
Phil Spitler (PRS) <phil@bloc.io>
2016-03-12 21:00:33 -0500

    added CORS 'NOTE: OPEN COMPLETELY BY DEFAULT'

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+}
+app.use(allowCrossDomain);
+
 var api = require('spitfire-express')('testapp', app.get('env'));
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +26,9 @@ var api = require('spitfire-express')('testapp', app.get('env'));
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -27,9 +37,9 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -37,23 +47,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.send({
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.send({
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send({
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.send({
+		message: err.message,
+		error: {}
+	});
 });
 
 
